3dcb9942ee8ea1e37624bea4f6f5c5e7afb15d76
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-03-11 22:04:25 -0700

    react tour

---
+++ src/components/countries/CountryData/CountryData.jsx
@@ -0,0 +1,12 @@
+
+
+const CountryData = ({country}) => {
+    console.log(country)
+    return (
+        <div>
+            <p><small>Country Data:{country}</small></p>
+        </div>
+    );
+};
+
+export default CountryData;
